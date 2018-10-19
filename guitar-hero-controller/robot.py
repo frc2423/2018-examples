a2332d368253754eb5ef50917573038efe4b6197
Amory Galili <amorygalili@gmail.com>
2018-10-19 16:38:33 -0400

    Added examples of how to control the robot with guitar hero, xbox, and thrustmaster controllers

---
+++ guitar-hero-controller/robot.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+    This is a good foundation to build your robot code on
+"""
+
+import wpilib
+import ctre
+
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.br_motor = ctre.wpi_talonsrx.WPI_TalonSRX(1)
+        self.bl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(2)
+        self.fl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(5)
+        self.fr_motor = ctre.wpi_talonsrx.WPI_TalonSRX(7)
+        self.fr_motor.setInverted(True)
+        self.br_motor.setInverted(True)
+
+        self.robot_drive = wpilib.RobotDrive(self.fl_motor, self.bl_motor, self.fr_motor, self.br_motor)
+
+        self.joystick = wpilib.Joystick(0)
+
+        self.l_arm = wpilib.Spark(0)
+        self.r_arm = wpilib.Spark(1)
+
+        self.elevator_motor = ctre.wpi_talonsrx.WPI_TalonSRX(4)
+        self.elevator_follower = ctre.wpi_talonsrx.WPI_TalonSRX(10)
+        self.elevator_follower.follow(self.elevator_motor)
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        pass
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        self.robot_drive.arcadeDrive(0, 0)
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        turn_rate = 0
+        if self.joystick.getRawButton(1):
+            turn_rate = -.75
+        elif self.joystick.getRawButton(6):
+            turn_rate = .75
+
+
+        self.robot_drive.arcadeDrive(turn_rate, -self.joystick.getRawAxis(1) * self.joystick.getRawAxis(3))
+
+
+        # elevator
+        elevator_speed = -0.2
+
+        if self.joystick.getRawButton(4):
+            # go down
+            elevator_speed = 0.35
+        elif self.joystick.getRawButton(2):
+            # go up
+            elevator_speed = -.8
+
+        self.elevator_motor.set(elevator_speed)
+
+        # arms
+        l_arm_speed = 0
+        r_arm_speed = 0
+
+        if self.joystick.getRawButton(3):
+            # intake
+            l_arm_speed = -.6
+            r_arm_speed = .6
+        elif self.joystick.getRawButton(5):
+            # outtake
+            l_arm_speed = .7
+            r_arm_speed = -.7
+
+        self.l_arm.set(l_arm_speed)
+        self.r_arm.set(r_arm_speed)
+
+
+
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
---
+++ xbox-one-controller/robot.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""
+    This is a good foundation to build your robot code on
+"""
+
+import wpilib
+import ctre
+
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.br_motor = ctre.wpi_talonsrx.WPI_TalonSRX(1)
+        self.bl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(2)
+        self.fl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(5)
+        self.fr_motor = ctre.wpi_talonsrx.WPI_TalonSRX(7)
+        self.fr_motor.setInverted(True)
+        self.br_motor.setInverted(True)
+
+        self.robot_drive = wpilib.RobotDrive(self.fl_motor, self.bl_motor, self.fr_motor, self.br_motor)
+
+        self.joystick = wpilib.Joystick(0)
+
+        self.l_arm = wpilib.Spark(0)
+        self.r_arm = wpilib.Spark(1)
+
+        self.elevator_motor = ctre.wpi_talonsrx.WPI_TalonSRX(4)
+        self.elevator_follower = ctre.wpi_talonsrx.WPI_TalonSRX(10)
+        self.elevator_follower.follow(self.elevator_motor)
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        pass
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        self.robot_drive.arcadeDrive(0, 0)
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        self.robot_drive.arcadeDrive(self.joystick.getRawAxis(0), self.joystick.getRawAxis(1))
+
+
+        # elevator
+        elevator_speed = -0.2
+        elevator_axis = self.joystick.getRawAxis(5)
+
+        if elevator_axis > .75:
+            # go down
+            elevator_speed = 0.35
+        elif elevator_axis < -.25:
+            # go up
+            elevator_speed = -.8
+
+        self.elevator_motor.set(elevator_speed)
+
+        # arms
+        l_arm_speed = 0
+        r_arm_speed = 0
+
+        if self.joystick.getRawButton(1):
+            # intake
+            l_arm_speed = -.6
+            r_arm_speed = .6
+        elif self.joystick.getRawButton(4):
+            # outtake
+            l_arm_speed = .7
+            r_arm_speed = -.7
+
+        self.l_arm.set(l_arm_speed)
+        self.r_arm.set(r_arm_speed)
+
+
+
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
