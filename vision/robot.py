17dc5f8a16f037e41ed74e6fba989b3059ed2a07
David <daviddanielian3@gmail.com>
2019-01-16 21:02:15 -0500

    Encoders going nuts idk mate

--- vision/robot.py
+++ vision/robot.py
@@ -5,6 +5,7 @@
 
 import wpilib
 import ctre
+import math
 from networktables import NetworkTables
 from networktables.util import ntproperty
 
@@ -22,6 +23,14 @@ class MyRobot(wpilib.TimedRobot):
     I = ntproperty('/PID/I', 0)
     D = ntproperty('/PID/D', 0)
 
+    ticks_per_rev_left = ntproperty('/encoders/ticks_per_rev_left', -1000)
+    ticks_per_rev_right = ntproperty('/encoders/ticks_per_rev', 1440)
+    max_speed = ntproperty('/encoders/max_speed', 10)
+
+    wheel_diameter = ntproperty('/encoders/wheel_diameter', 6)
+
+
+
 
     image_center = 15
 
@@ -30,14 +39,37 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
+        self.rev_per_ft = 12 / (math.pi * self.wheel_diameter)
+
+        self.ticks_per_ft_left = self.rev_per_ft * self.ticks_per_rev_left
+        self.ticks_per_ft_right = self.rev_per_ft * self.ticks_per_rev_right
+
+
         self.br_motor = ctre.wpi_talonsrx.WPI_TalonSRX(1)
         self.bl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(2)
         self.fl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(5)
         self.fr_motor = ctre.wpi_talonsrx.WPI_TalonSRX(7)
         self.fr_motor.setInverted(True)
         self.br_motor.setInverted(True)
+        self.br_motor.follow(self.fr_motor)
+        self.bl_motor.follow(self.fl_motor)
 
-        self.robot_drive = wpilib.RobotDrive(self.fl_motor, self.bl_motor, self.fr_motor, self.br_motor)
+
+        self.fr_motor.config_kP(1, 1, 0)
+        self.fr_motor.config_kI(1, 0, 0)
+        self.fr_motor.config_kD(1, 0, 0)
+
+        self.fl_motor.config_kP(1, 1, 0)
+        self.fl_motor.config_kI(1, 0, 0)
+        self.fl_motor.config_kD(1, 0, 0)
+
+        self.fr_motor.configSelectedFeedbackSensor(ctre.FeedbackDevice.CTRE_MagEncoder_Relative, 1, 0)
+        self.fr_motor.setSensorPhase(False)
+
+
+        self.fl_motor.configSelectedFeedbackSensor(ctre.FeedbackDevice.CTRE_MagEncoder_Relative, 1, 0)
+
+        #self.robot_drive = wpilib.RobotDrive(self.fl_motor, self.bl_motor, self.fr_motor, self.br_motor)
 
         self.joystick = wpilib.Joystick(0)
 
@@ -69,13 +101,31 @@ class MyRobot(wpilib.TimedRobot):
         # make something happen here
         self.turn_rate = -output
 
+    def get_left_angular_pos(self):
+      encoder_value = self.fl_motor.getQuadraturePosition()
+      ticks_per_turn = -1000
+      return 2 * math.pi * encoder_value / ticks_per_turn
+
+
+    def get_right_angular_pos(self):
+        encoder_value = self.br_motor.getQuadraturePosition()
+        ticks_per_turn = 1440
+        return 2 * math.pi * encoder_value / ticks_per_turn
+
+    def to_motor_speed(self, ft_per_second, ticks_per_rev):
+        ticks_per_ft = ticks_per_rev * self.rev_per_ft
+        ticks_per_sec = ft_per_second* ticks_per_ft
+        return ticks_per_sec * .1
+
+
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
         pass
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-        self.robot_drive.arcadeDrive(0, 0)
+        #self.robot_drive.arcadeDrive(0, 0)
+        pass
 
     def teleopInit(self):
         limelight_table = NetworkTables.getTable("limelight")
@@ -87,6 +137,21 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
 
+        left_speed = -self.joystick.getRawAxis(1) * self.max_speed
+        left_motor_speed = self.to_motor_speed(left_speed, self.ticks_per_rev_left)
+
+        print('left_motor_speed:', left_motor_speed)
+
+
+        self.fl_motor.set(ctre.WPI_TalonSRX.ControlMode.Velocity, left_motor_speed)
+
+        print('encoder', self.fl_motor.getQuadraturePosition())
+
+        right_speed = -self.joystick.getRawAxis(5) * self.max_speed
+        right_motor_speed = self.to_motor_speed(right_speed, self.ticks_per_rev_right)
+        self.fr_motor.set(ctre.WPI_TalonSRX.ControlMode.Velocity, right_motor_speed)
+
+        return
 
         if self.target_found:
             self.last_seen = self.timer.getMsClock()
