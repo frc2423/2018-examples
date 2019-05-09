cadf21b1ec721ab2eb0921664f23a488dbdd7e06
DavidD121 <daviddanielian3@gmail.com>
2019-05-09 19:47:22 -0400

    working limelight following reflective tape code

--- limelight/robot.py
+++ limelight/robot.py
@@ -18,6 +18,13 @@ class MyRobot(wpilib.TimedRobot):
     ta = ntproperty('/limelight/ta', 0)
     tx = ntproperty('/limelight/tx', 0)
 
+    min_tx = ntproperty('/limelight/min_tx', 0, persistent = True)
+    max_tx = ntproperty('/limelight/max_tx', 0, persistent = True)
+    min_ta = ntproperty('/limelight/min_ta', 1, persistent = True)
+    max_ta = ntproperty('/limelight/max_ta', 15, persistent = True)
+
+    speed_multiplier = ntproperty('/limelight/speed_multiplier', 1, persistent = True)
+    turn_multiplier = ntproperty('/limelight/turn_multiplier', 1, persistent = True)
     def robotInit(self):
         """
         This function is called upon program startup and
@@ -34,9 +41,6 @@ class MyRobot(wpilib.TimedRobot):
 
         self.joystick = wpilib.Joystick(0)
 
-        self.min_ta = 3
-        self.max_ta = 11
-
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
         pass
@@ -49,17 +53,19 @@ class MyRobot(wpilib.TimedRobot):
         """This function is called periodically during operator control."""
 
         if self.ta > self.min_ta and self.ta < self.max_ta and self.tv:
+          #changing target area to a value between -.5 to .5 in order to plug into arcade drive
           max_speed = self.max_ta - self.min_ta
-          divide_speed = (self.max_ta - self.min_ta) * 2
-          speed = (max_speed - self.ta) / divide_speed
+          divide_speed = (self.max_ta - self.min_ta)
+          speed = ((max_speed - self.ta) / divide_speed) - .5
 
           print('speed: ', speed)
 
-          self.robot_drive.arcadeDrive(speed, 0)
+          self.robot_drive.arcadeDrive(self.tx * self.turn_multiplier / 20, -speed * self.speed_multiplier)
 
         else:
           print('target not detected')
-          self.robot_drive.arcadeDrive(0, 0)
+          self.robot_drive.arcadeDrive(self.joystick.getX(), self.joystick.getY())
+
 
 
 if __name__ == "__main__":
